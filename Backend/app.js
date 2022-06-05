a76e2631d2d812e83f9360c9ee95d79abb947d4f
saurabhku463 <saurabhku463@gmail.com>
2022-06-05 14:50:49 +0530

    fix: minor

--- Backend/app.js
+++ Backend/app.js
@@ -15,8 +15,8 @@ const helmet = require('helmet');
 // middlewares
 app.use(morgan('tiny'));
 
-app.use(express.json({ limit: '1mb' }));
-app.use(express.urlencoded({ limit: '1mb', extended: true }));
+app.use(express.json({ limit: '5mb' }));
+app.use(express.urlencoded({ limit: '5mb', extended: true }));
 
 const corsOption = {
     origin: process.env.CLIENT_URL,

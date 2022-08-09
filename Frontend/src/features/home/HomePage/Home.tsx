805dc31908a1230b4ee5700b2bf62bb470a16433
saurabhku463 <saurabhku463@gmail.com>
2022-08-09 23:12:50 +0530

    fix:  added loader on post fetch

--- Frontend/src/features/home/HomePage/Home.tsx
+++ Frontend/src/features/home/HomePage/Home.tsx
@@ -1,11 +1,12 @@
 import './Components/Home.css';
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useAppDispatch, useAppSelector } from '../../../store/hooks';
 
 import { LeftSideBar, RightSideBar } from '../../index';
 import Posts from '../../../Components/Posts/Posts';
 import Loader from '../../../Components/Loader/LoaderMain';
+import ContainerLoader from '../../../Components/Loader/ContainerLoader';
 
 import useScrollToTop from '../../../Hooks/useScrollToTop';
 
@@ -21,16 +22,23 @@ const Home = () => {
     const { posts, postStatus } = useAppSelector((state) => state.post);
     const dispatch = useAppDispatch();
     const { reactionStatus } = useAppSelector((state) => state.post);
+    const [getPostState, setGetPostStatus] = useState<
+        'IDLE' | 'PENDING' | 'FETCHED'
+    >('IDLE');
 
     useEffect(() => {
         const controller = new AbortController();
         let unMounted = false;
 
+        setGetPostStatus('PENDING');
         dispatch(getAllPosts({ controller, unMounted }));
+
+        setGetPostStatus('FETCHED');
 
         return () => {
             controller.abort();
             unMounted = true;
+            setGetPostStatus('IDLE');
         };
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
@@ -57,28 +65,31 @@ const Home = () => {
                 <div className="container-sidebar">
                     <LeftSideBar />
                 </div>
-                <div className="container-post container ">
-                    <ul>
-                        {posts.map((post: SmallPost) => (
-                            <Posts
-                                image={post?.image?.secure_url}
-                                heading={post.title}
-                                id={post.post_id}
-                                authorImage={
-                                    post.author.profile_photo.secure_url
-                                }
-                                authorName={post.author.name}
-                                authorUsername={post.author.username}
-                                postDate={post.createdAt}
-                                numberOfComments={post.comments.length}
-                                numberOfLikes={post.likes.length}
-                                postDescription={post.description}
-                                key={post.post_id}
-                                tagsArray={post.tags}
-                                bookmarkArray={post.bookmarks}
-                            />
-                        ))}
-                    </ul>
+                <div className="container-post container">
+                    {getPostState === 'FETCHED' && (
+                        <ul>
+                            {posts.map((post: SmallPost) => (
+                                <Posts
+                                    image={post?.image?.secure_url}
+                                    heading={post.title}
+                                    id={post.post_id}
+                                    authorImage={
+                                        post.author.profile_photo.secure_url
+                                    }
+                                    authorName={post.author.name}
+                                    authorUsername={post.author.username}
+                                    postDate={post.createdAt}
+                                    numberOfComments={post.comments.length}
+                                    numberOfLikes={post.likes.length}
+                                    postDescription={post.description}
+                                    key={post.post_id}
+                                    tagsArray={post.tags}
+                                    bookmarkArray={post.bookmarks}
+                                />
+                            ))}
+                        </ul>
+                    )}
+                    {getPostState === 'PENDING' && <ContainerLoader />}
                 </div>
                 <RightSideBar />
             </div>
